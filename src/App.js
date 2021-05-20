d62ffb2b227f70e737eab34c086496f950591e2a
Signior-X <b19188@students.iitmandi.ac.in>
2021-05-20 21:06:59 +0530

    Design header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/img/wall3.jpg'})`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover"
+    }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="left">
+          <div className="option">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="option">File</div>
+          <div className="option">Edit</div>
+          <div className="option">View</div>
+          <div className="option">Go</div>
+        </div>
+        <div className="right">
+          <div className="option">
+            <img src="img/arrow-down.png" style={{ width: "20px" }} />
+          </div>
+          <div className="option">
+            <div class="battery">
+              <div class="body">
+                <div class="charging"></div>
+              </div>
+              <div class="terminal"></div>
+            </div>
+          </div>
+          <div className="option">12:00 AM</div>
+        </div>
+      </header>
+      <main>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +45,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </main>
     </div>
   );
 }
