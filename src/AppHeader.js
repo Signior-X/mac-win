47bc8a967889176898ce85a21681984459d47db7
Signior-X <b19188@students.iitmandi.ac.in>
2021-05-21 12:55:30 +0530

    Created realtime battery

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -2,6 +2,56 @@ import React from "react";
 
 import logo from './logo.svg';
 
+class Battery extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      batteryPercentage: 0.5
+    }
+
+    // Global reference variables
+    this.myBattery = {};
+    this.myBatteryListener = this.batteryPercentChangeListener;
+  }
+
+  batteryPercentChangeListener = () => {
+    console.log("Battery change", this.myBattery.level);
+
+    this.setState({ batteryPercentage: this.myBattery.level });
+  }
+
+  componentDidMount() {
+    navigator.getBattery().then((bat) => {
+      console.log("Current Battery Level", bat.level);
+      this.setState({ batteryPercentage: bat.level });
+
+      // Storing a reference to myBattery
+      this.myBattery = bat;
+
+      bat.addEventListener('levelchange', this.myBatteryListener);
+    });
+  }
+
+  componentWillUnmount() {
+    this.myBattery.removeEventListener('levelchange', this.myBatteryListener);
+  }
+
+  render() {
+    const batteryPercentage = this.state.batteryPercentage;
+    const batteryTranslateX = (1 - batteryPercentage) * 100;
+
+    return (
+      <div className="battery">
+        <div className="body">
+          <div className="charging" style={{ transform: `translateX(-${batteryTranslateX}%)`, color: "blue" }}></div>
+        </div>
+        <div className="terminal"></div>
+      </div>
+    )
+  }
+}
+
 class AppHeader extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -28,12 +78,7 @@ class AppHeader extends React.PureComponent {
             <img src="img/arrow-down.png" alt="Show dropdown menu" style={{ width: "20px" }} />
           </div>
           <div className="option">
-            <div className="battery">
-              <div className="body">
-                <div className="charging"></div>
-              </div>
-              <div className="terminal"></div>
-            </div>
+            <Battery />
           </div>
           <div className="option">12:00 AM</div>
         </div>
